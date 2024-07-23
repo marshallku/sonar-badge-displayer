39983d4795a6888652d2abaf4461edf9544c0cc5
Marshall Ku <marshall@kakao.com>
2024-07-23 15:20:08 +0900

    Add host and port in env

--- src/env/env.rs
+++ src/env/env.rs
@@ -4,6 +4,8 @@ use std::borrow::Cow;
 
 #[derive(Clone, Debug)]
 pub struct Env {
+    pub host: Cow<'static, str>,
+    pub port: Cow<'static, str>,
     pub sonarqube_url: Cow<'static, str>,
     pub sonarqube_token: Cow<'static, str>,
 }
@@ -20,8 +22,18 @@ impl Env {
             Ok(url) => Cow::Owned(url),
             Err(_) => panic!("SONARQUBE_TOKEN is not set"),
         };
+        let host = match std::env::var("HOST") {
+            Ok(url) => Cow::Owned(url),
+            Err(_) => Cow::Borrowed("127.0.0.1"),
+        };
+        let port = match std::env::var("PORT") {
+            Ok(url) => Cow::Owned(url),
+            Err(_) => Cow::Borrowed("3000"),
+        };
 
         Self {
+            host,
+            port,
             sonarqube_url,
             sonarqube_token,
         }
